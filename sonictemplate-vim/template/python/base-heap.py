34314951f77c2b9a7d01404dc712089d06a9f81d
b4sh5i <lolwinz@naver.com>
2019-06-29 01:00:08 -0700

    aslr set

--- sonictemplate-vim/template/python/base-heap.py
+++ sonictemplate-vim/template/python/base-heap.py
@@ -1,7 +1,7 @@
 from b4sh5i import *
 
 proc = "./{{_expr_:substitute('{{_input_:name}}', '\w\', '\u\0', '')}}"
-s = process(proc)
+s = process(proc, aslr=False)
 e = ELF(proc)
 gdb.attach(s, 'tracemalloc on\nc')
 
--- sonictemplate-vim/template/python/base-pie.py
+++ sonictemplate-vim/template/python/base-pie.py
@@ -1,7 +1,7 @@
 from b4sh5i import *
 
 proc = "./{{_expr_:substitute('{{_input_:name}}', '\w\', '\u\0', '')}}"
-s = process(proc)
+s = process(proc, aslr=False)
 e = ELF(proc)
 gdb_pie_attach(s, [{{_cursor_}}],'tracemalloc on\n')
 
